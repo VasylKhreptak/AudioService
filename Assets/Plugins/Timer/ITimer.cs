75b5a0fd32dcc384319adcfa7de4eca51b229cbb
VASYL\chrep <vasylkhreptak12@gmail.com>
2024-01-14 20:07:26 +0200

    - imported Timer; - imported UniRx;

---
+++ Assets/Plugins/Timer/ITimer.cs
@@ -0,0 +1,25 @@
+﻿namespace Plugins.Timer
+{
+    public interface ITimer : IReadonlyTimer
+    {
+        public void Start(float duration);
+
+        public void Complete();
+
+        public void Stop();
+
+        public void Pause();
+
+        public void Resume();
+
+        public void TogglePause();
+
+        public void Reset();
+
+        public void SetTimeScale(float timeScale);
+
+        public void SetTime(float time);
+
+        public void SetTargetTime(float targetTime);
+    }
+}
