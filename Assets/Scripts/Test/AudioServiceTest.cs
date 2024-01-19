44d84a0530648fb26f6b025c864d0d84d0c5c8eb
VASYL\chrep <vasylkhreptak12@gmail.com>
2024-01-19 10:44:25 +0200

    - added possibility to change audio time;

--- Assets/Scripts/Test/AudioServiceTest.cs
+++ Assets/Scripts/Test/AudioServiceTest.cs
@@ -1,4 +1,5 @@
 ﻿using Plugins.AudioService.Core;
+using Plugins.Timer;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using Zenject;
@@ -24,28 +25,55 @@ namespace Test
 
         #region MonoBehaviour
 
+        // private void Awake()
+        // {
+        //     AudioSource audioSource;
+        //     
+        //     audioSource.
+        // }
+
         private void Update()
         {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                _audioService.Time.TrySet(_audioID, 0);
+            }
+
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 _audioID = _audioService.Play(_clip, _settings);
+                Debug.Log("Play");
             }
+
+            if (_audioService.Timer.TryGet(_audioID, out IReadonlyTimer timer))
+                Debug.Log($"Time: {timer.Time}");
         }
 
         #endregion
 
+        [Button] private void AddSecond() => AddTime(1);
+        [Button] private void RemoveSecond() => AddTime(-1);
+        [Button] private void SetSecond() => SetTime(1);
         [Button] private void Stop() => _audioService.Stop(_audioID);
-        [Button] void StopAll() => _audioService.StopAll();
-        [Button] void StopNotLooped() => _audioService.Stop(audio => audio.Loop == false);
-        [Button] void Pause() => _audioService.Pause(_audioID);
-        [Button] void PauseAll() => _audioService.PauseAll();
-        [Button] void PauseNotLooped() => _audioService.Pause(audio => audio.Loop == false);
-        [Button] void Resume() => _audioService.Resume(_audioID);
-        [Button] void ResumeAll() => _audioService.ResumeAll();
-        [Button] void ResumeNotLooped() => _audioService.Resume(audio => audio.Loop == false);
-        [Button] void IsActive() => Debug.Log(_audioService.IsActive(_audioID));
-        [Button] void ActiveAudiosCount() => Debug.Log(_audioService.ActiveAudiosCount());
-        [Button] void ActiveLoopedAudiosCount() => Debug.Log(_audioService.ActiveAudiosCount(audio => audio.Loop));
-        [Button] void ApplySettings() => _audioService.ApplySettings(_audioID, _settings);
+        [Button] private void StopAll() => _audioService.StopAll();
+        [Button] private void StopNotLooped() => _audioService.Stop(audio => audio.Loop == false);
+        [Button] private void Pause() => _audioService.Pause(_audioID);
+        [Button] private void PauseAll() => _audioService.PauseAll();
+        [Button] private void PauseNotLooped() => _audioService.Pause(audio => audio.Loop == false);
+        [Button] private void Resume() => _audioService.Resume(_audioID);
+        [Button] private void ResumeAll() => _audioService.ResumeAll();
+        [Button] private void ResumeNotLooped() => _audioService.Resume(audio => audio.Loop == false);
+        [Button] private void IsActive() => Debug.Log(_audioService.IsActive(_audioID));
+        [Button] private void ActiveAudiosCount() => Debug.Log(_audioService.ActiveAudiosCount());
+        [Button] private void ActiveLoopedAudiosCount() => Debug.Log(_audioService.ActiveAudiosCount(audio => audio.Loop));
+        [Button] private void ApplySettings() => _audioService.ApplySettings(_audioID, _settings);
+
+        private void SetTime(float time) => _audioService.Time.TrySet(_audioID, time);
+
+        private void AddTime(float time)
+        {
+            if (_audioService.Time.TryGet(_audioID, out float currentTime))
+                _audioService.Time.TrySet(_audioID, currentTime + time);
+        }
     }
 }
